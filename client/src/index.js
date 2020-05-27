171b8b65a9098d56bc163e98b55defb20dff0736
Kody Maus <auto.mausx@gmail.com>
2020-05-27 10:06:33 -0600

    add conditional to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import Footer from './Footer';
 
-ReactDOM.render(<App />, document.getElementById("Header"));
-ReactDOM.render(<Footer />, document.getElementById('Footer'));
+if (typeof window !== undefined) {
+  ReactDOM.render(<App />, document.getElementById("Header"));
+  ReactDOM.render(<Footer />, document.getElementById('Footer'));
+}
 
 
