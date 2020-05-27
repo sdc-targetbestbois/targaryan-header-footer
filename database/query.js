449fc1c21115b2a6fe20089a5e2ff49e6115afc6
Kody Maus <auto.mausx@gmail.com>
2020-05-27 08:01:15 -0600

    comment out require config

--- database/query.js
+++ database/query.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const config = require('./config');
+// const config = require('./config');
 
 mongoose.connect("mongodb://127.0.0.1:27017/targaryan", {useNewUrlParser: true})
 .then(() => {
