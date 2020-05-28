b2592158e38c4612673e321b6d8873b0a94bac45
Kody Maus <auto.mausx@gmail.com>
2020-05-28 11:05:53 -0600

    update mongoose connect with deployed db

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import Footer from './Footer';
 
-if (typeof window !== undefined) {
+if (typeof window !== 'undefined') {
   ReactDOM.render(<App />, document.getElementById("Header"));
   ReactDOM.render(<Footer />, document.getElementById('Footer'));
 }
--- database/query.js
+++ database/query.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 // const config = require('./config');
 
-mongoose.connect("mongodb://127.0.0.1:27017/targaryan", {useNewUrlParser: true})
+mongoose.connect("mongodb://ec2-18-224-38-14.us-east-2.compute.amazonaws.com/targaryan", {useNewUrlParser: true})
 .then(() => {
   console.log("database connected!")
 })
